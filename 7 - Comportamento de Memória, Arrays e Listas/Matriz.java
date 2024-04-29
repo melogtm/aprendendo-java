e30f9d959f1cd68c2f52d65ec77a63898f6d1e3f
gabrieltrigueiro <gabrielcursos@proton.me>
2024-04-29 16:47:34 -0300

    matrizes

--- 7 - Comportamento de Memória, Arrays e Listas/Matriz.java
+++ 7 - Comportamento de Memória, Arrays e Listas/Matriz.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
+import java.util.Locale; 
+
 public class Matriz {
-    
+    public static void main(String args[]) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in); 
+        
+        int n = sc.nextInt();
+        int[][] mat = new int[n][n];
+
+        int countNegative = 0; 
+        
+        // Lendo a matriz
+        for (int i = 0; i < mat.length; i++) { // Linhas
+            for (int j = 0; j < mat[i].length; j++) { // Colunas
+                mat[i][j] = sc.nextInt(); 
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("Diagonal Principal: ");
+
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print(mat[i][i] + " ");
+        }
+
+        for (int i = 0; i < mat.length; i++) {
+            // mat[i].length = número de colunas da linha i
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < 0) {
+                    countNegative++;
+                }
+            }
+        }
+
+        System.out.printf("Números negativos: %d\n", countNegative);
+
+        sc.close(); 
+    }
 }
